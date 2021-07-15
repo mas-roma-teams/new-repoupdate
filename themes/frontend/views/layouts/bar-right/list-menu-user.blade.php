7fd12dd56693f0c995b79f52f6535d0fb7d6045b
instrukturbaba <sudarmibindarum1@gmail.com>
2021-07-16 01:08:52 +0700

    update nego

--- themes/frontend/views/layouts/bar-right/list-menu-user.blade.php
+++ themes/frontend/views/layouts/bar-right/list-menu-user.blade.php
@@ -32,6 +32,11 @@
               <!-- <span class="badge badge-primary badge-pill">2</span> -->
             </li>
 
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+             <a href="{{ route('layout-negosiasi') }}"> Status Negosiasi</a>
+              <!-- <span class="badge badge-primary badge-pill">2</span> -->
+            </li>
+
 
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="{{ route('users.password') }}"> Ganti password</a>
